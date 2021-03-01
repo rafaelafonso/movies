d904fdca13e117a45e4763f95cb31a7b1338abda
rafaelafonso <rafaelafonso1980@gmail.com>
2021-03-01 00:17:04 +0000

    Add image loader

--- Movies/Movies/Model/ImageLoader.swift
+++ Movies/Movies/Model/ImageLoader.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import Combine
+
+class ImageLoader: ObservableObject {
+
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+
+    init(string: String) {
+        guard let url = URL(string: string) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
